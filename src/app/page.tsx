4bc5c3c1a9405877be9bb3552964d7c74f455333
Aggarwal07 <shivamaggarwal179@gmail.com>
2024-04-24 12:28:15 +0530

    2more Sec -home , navbar responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,13 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 export default function Home() {
+  //FeaturedDrops List
+  const featuredDrops = [
+    { id: "1", imageLink: "/Images/home/1.webp" },
+    { id: "2", imageLink: "/Images/home/1.webp" },
+    { id: "3", imageLink: "/Images/home/1.webp" },
+    { id: "4", imageLink: "/Images/home/1.webp" },
+  ];
   var settings = {
     dots: true,
     infinite: true,
@@ -16,6 +23,14 @@ export default function Home() {
     autoplaySpeed: 3000,
     pauseOnHover: true,
     arrows: false,
+  };
+  var settingsProducts = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
   };
   return (
     <div>
@@ -59,18 +74,80 @@ export default function Home() {
           <div>shivam</div>
         </Slider>
       </div>
-      {/*2nd design */}
+      {/*2nd division */}
       <div className="w-full bg-gray-100 mt-24 py-8">
-        <p className="text-[2em] text-center">Find the perfect custom canvas for you</p>
+        <p className="text-[2em] text-center">
+          Find the perfect custom canvas for you
+        </p>
         <div className="w-[40vw] mx-auto flex justify-between mt-10">
-          <button style={{
-            boxShadow: '0px 4px 4px rgba(255, 0, 0, 0.5)',
-          }} className="rounded-xl w-[12em] h-[3.5em] text-center text-white bg-black">Explore Collection</button>
-          <button style={{
-            boxShadow: '0px 4px 4px rgba(0, 0, 255, 0.5)',
-          }} className="rounded-xl w-[12em] h-[3.5em] text-center text-white bg-black">Design Your Own</button>
+          <button
+            style={{
+              boxShadow: "0px 4px 4px rgba(255, 0, 0, 0.5)",
+            }}
+            className="rounded-xl w-[12em] h-[3.5em] text-center text-white bg-black"
+          >
+            Explore Collection
+          </button>
+          <button
+            style={{
+              boxShadow: "0px 4px 4px rgba(0, 0, 255, 0.5)",
+            }}
+            className="rounded-xl w-[12em] h-[3.5em] text-center text-white bg-black"
+          >
+            Design Your Own
+          </button>
         </div>
+      </div>
+      {/*Category Slider */}
+      <div className="bg-black mt-10 py-2">
+        <div className="mx-5 my-5">
+          <Slider {...settingsProducts}>
+            {featuredDrops.map((index, item) => (
+              <div className="w-[23vw] h-[45vh] bg-red-600 outline relative">
+                <Image
+                  style={{
+                    objectFit: "cover",
+                    height: "100%",
+                  }}
+                  alt="polaroid"
+                  src="/Images/home/1.webp"
+                  width={1600}
+                  height={1600}
+                />
+                <p className="text-white text-2xl absolute top-[50%] left-[46%] uppercase">
+                  text
+                </p>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
 
+      {/*product categories*/}
+      <p className="text-[#503114] text-2xl font-semibold mt-10 text-center">
+        FEATURED DROPS
+      </p>
+      <div className="w-fit gap-5 grid grid-cols-4 mx-auto mt-10 mb-10">
+        {featuredDrops.map((item, index) => (
+          <div className="" key={index}>
+            <div className="w-[23vw] h-[23vw]">
+              <Image
+                style={{
+                  objectFit: "cover",
+                  height: "100%",
+                }}
+                alt="polaroid"
+                src="/Images/home/1.webp"
+                width={1600}
+                height={1600}
+              />
+            </div>
+            <p className="text-center mt-5 text-lg">Name</p>
+            <p className="text-center text-sm text-gray-400 font-light">
+              Price
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,15 +1,21 @@
+"use client";
 import React from "react";
 import { CiSearch } from "react-icons/ci";
 import { BsBag } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
 import Image from "next/image";
+import { useState } from "react";
+import Drawer from "react-modern-drawer";
+import "react-modern-drawer/dist/index.css";
 
 const Navbar = () => {
+  const [toggleMore, setMore] = useState(false);
   return (
     <div className=" font-light">
       <div className=" bg-[#1a1109] text-center text-[11px] font-bold py-2 text-white">
         FREE SHIPPPING PAN INDIA
       </div>
-      <div className="flex items-center justify-between w-full h-[3.4em] px-24 mt-3">
+      <div className="flex items-center justify-between w-full h-[3.4em] px-2 md:px-24 mt-3">
         <div className=" cursor-pointer">
           <Image
             src="/Images/logo/logo.svg"
@@ -18,10 +24,41 @@ const Navbar = () => {
             height={55}
           />
         </div>
-        <div className="flex text-sm items-center">
-          <div className="h-full px-3 cursor-pointer">
-            <CiSearch size={22} />
+        {/*menue for mobile*/}
+        <div className="sm:hidden">
+          <div
+            onClick={() => {
+              toggleMore ? setMore(false) : setMore(true);
+            }}
+          >
+            <GiHamburgerMenu size={28}/>
           </div>
+          <Drawer
+            open={toggleMore}
+            onClose={() => {
+              toggleMore ? setMore(false) : setMore(true);
+            }}
+            direction="right"
+            className=""
+            size="70%"
+            style={{
+              background: "linear-gradient(to bottom, #f5f5f5, #ffffff)",
+            }}
+          >
+            <div className="flex flex-col w-fit mx-auto text-center text-xl mt-52">
+              <div className="p-2">Home</div>
+              <div className="p-2">Collection</div>
+              <div className="p-2">Design Your Own</div>
+              <div className="p-2">More</div>
+              <div className="p-2">Bag</div>
+            </div>
+          </Drawer>
+        </div>
+        {/*menue for desktop */}
+        <div className="flex text-sm items-center max-sm:hidden">
+          {/* <div className="h-full px-3 cursor-pointer">
+            <CiSearch size={22} />
+          </div> */}
           <div className="border-r-2 h-full px-3 cursor-pointer">Home</div>
           <div className="border-r-2 h-full px-3 cursor-pointer">
             Collection
