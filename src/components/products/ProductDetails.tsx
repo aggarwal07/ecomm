0f172d3312b22f54fb67bf57ce8a556a13a68447
Aggarwal07 <shivamaggarwal179@gmail.com>
2024-04-26 16:25:22 +0530

    imageGen Commented

--- src/components/products/ProductDetails.tsx
+++ src/components/products/ProductDetails.tsx
@@ -7,7 +7,7 @@ import ProductCard from "./ProductCard";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import ImageGen from "./ImageGen";
+// import ImageGen from "./ImageGen";
 interface ProductDetails {
   productName: string;
 }
