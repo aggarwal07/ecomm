69830860684e5621b52561bc5b32d73cd0a2a57c
Aggarwal07 <shivamaggarwal179@gmail.com>
2024-04-29 19:15:35 +0530

    react-redux Store setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Provider } from 'react-redux'
+import { store } from "@/store/store";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    
     <html lang="en">
+      <Provider store={store}>
       <body className={inter.className}>{children}</body>
+    </Provider>
     </html>
   );
 }
