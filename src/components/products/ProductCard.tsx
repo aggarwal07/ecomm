e23dd93f824ca117c2ae5750213c27c5d0e08d4c
Aggarwal07 <shivamaggarwal179@gmail.com>
2024-04-29 18:46:01 +0530

    api fetch code added

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -1,7 +1,24 @@
 import React from "react";
 import Image from "next/image";
-
+import { useEffect, useState } from "react";
 const ProductCard = () => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://ecomm-backend-3z6a.onrender.com/api/products");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
+  }, []);
+  console.log(data);
   return (
     <div className="w-fit">
       <div className="md:w-[23em] md:h-[23em] w-[12.5em] h-[12.5em]">
