7f318247f349762b7826823d99bb53c86b42d5cf
Aggarwal07 <shivamaggarwal179@gmail.com>
2024-05-08 01:32:52 +0530

    ui update

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,5 +1,5 @@
-'use client'
-import React from 'react'
+"use client";
+import React from "react";
 import Image from "next/image";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -14,72 +14,86 @@ import { useAppDispatch } from "@/store/hooks";
 import { Product } from "@/types/types";
 
 const LandingSection = () => {
-    const dispatch = useAppDispatch();
-    //Product Api data fetching
-    const [ProductData, setProdData] = useState<Product[] | null>(null);
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch(
-            "https://backendfiggle.onrender.com/api/products"
-          );
-          if (!response.ok) {
-            throw new Error("Network response was not ok");
-          }
-          const jsonData = await response.json();
-          setProdData(jsonData);
-          dispatch(setProducts(jsonData));
-        } catch (error) {
-          console.error("Error fetching data:", error);
+  const dispatch = useAppDispatch();
+  //Product Api data fetching
+  const [ProductData, setProdData] = useState<Product[] | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(
+          "https://backendfiggle.onrender.com/api/products"
+        );
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
         }
-      };
-      fetchData();
-    }, []);
-    console.log(ProductData);
-    //router
-    const router = useRouter();
-    //FeaturedDrops List
-    const featuredDrops = [
-      { id: "1", imageLink: "/Images/home/1.webp" },
-      { id: "2", imageLink: "/Images/home/1.webp" },
-      { id: "3", imageLink: "/Images/home/1.webp" },
-      { id: "4", imageLink: "/Images/home/1.webp" },
-    ];
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      autoplay: true,
-      autoplaySpeed: 3000,
-      pauseOnHover: true,
-      arrows: false,
+        const jsonData = await response.json();
+        setProdData(jsonData);
+        dispatch(setProducts(jsonData));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
     };
-    var settingsProducts = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      arrows: true,
-      responsive: [
-        {
-          breakpoint: 786,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
-            infinite: true,
-            dots: true,
-            centerMode: true,
-          },
+    fetchData();
+  }, []);
+  console.log(ProductData);
+  //router
+  const router = useRouter();
+  //FeaturedDrops List
+  const featuredDrops = [
+    { id: "1", imageLink: "/Images/home/1.webp" },
+    { id: "2", imageLink: "/Images/home/1.webp" },
+    { id: "3", imageLink: "/Images/home/1.webp" },
+    { id: "4", imageLink: "/Images/home/1.webp" },
+  ];
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    pauseOnHover: true,
+    arrows: false,
+  };
+  var settingsProducts = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    responsive: [
+      {
+        breakpoint: 786,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true,
+          centerMode: true,
         },
-      ],
-    };
+      },
+    ],
+  };
   return (
     <div>
-        {/*Polaroid */}
-        <div
+      {/*landing image */}
+      <div className="w-full h-[80vh]">
+        <Image
+          style={{
+            objectFit: "cover",
+            height: "100%",
+            width : "100%",
+          }}
+          alt="polaroid"
+          src="/Images/home/landing.jpg"
+          width={1600}
+          height={1600}
+        />
+      </div>
+      {/*Polaroid */}
+      <div
         style={{
           boxShadow: "0 25px 50px -12px rgba(0, 0, 0, 0.75)",
         }}
@@ -200,7 +214,7 @@ const LandingSection = () => {
       {/*contanct action button */}
       <ContactUs />
     </div>
-  )
-}
+  );
+};
 
-export default LandingSection
+export default LandingSection;
