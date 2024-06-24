893f0650c129b7f9f960aed61945f44a132597e3
Aggarwal07 <shivamaggarwal179@gmail.com>
2024-06-24 17:34:18 +0530

    product card responsive

--- src/components/products/ProductCard.tsx
+++ src/components/products/ProductCard.tsx
@@ -37,7 +37,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
           var query = "/products/" + product._id;
           router.push(query);
         }}
-        className="md:w-[15.5em] md:h-[19.5em] w-[10.5em] h-[11.5em] "
+        className="md:w-[15.5em] md:h-[19.5em] w-[10.5em] h-[14.2em] overflow-hidden "
       >
         <Image
           style={{
@@ -48,22 +48,22 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
           src={product.images[0]} // Assuming only one image for simplicity
           width={1600}
           height={1600}
-          className=""
+          className="max-md:scale-110"
         />
         {product.maxPrice && (
-          <div className="max-sm:text-sm border-b border-r absolute top-0 left-0 p-1 italic px-4 bg-black">
+          <div className="max-md:text-xs border-b border-r absolute top-0 left-0 p-1 italic px-4 bg-black">
             SALE
           </div>
         )}
       </div>
-      <div className="flex flex-col px-2 py-2 lg:px-3 lg:py-4 bg-[#e9e9e9] text-gray-800 h-[11.5em]">
+      <div className="flex flex-col px-2 py-2 lg:px-3 lg:py-4 bg-[#e9e9e9] text-gray-800 h-[10em] md:h-[11.5em]">
         <p
           onClick={() => {
             var query = "/products/" + product._id;
             router.push(query);
           }}
           style={{ lineHeight: "18px" }}
-          className=" text-sm font-semibold"
+          className="text-xs md:text-sm font-semibold line-clamp-2"
         >
           {product.name} | {product.type[0].size}
         </p>
@@ -72,18 +72,18 @@ const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
             var query = "/products/" + product._id;
             router.push(query);
           }}
-          className="flex sm:flex-col max-sm:items-center max-sm:gap-2 mt-4"
+          className="flex-col max-sm:items-center max-sm:gap-2 mt-3 md:mt-4"
         >
           {product.maxPrice && (
-            <p className=" line-through text-sm text-gray-500 ">
+            <p className=" line-through text-xs md:text-sm text-gray-500 ">
               Rs.{product.maxPrice}.00
             </p>
           )}
-          <p className=" text-md text-gray-600">From Rs.{product.price}.00</p>
+          <p className="text-sm md:text-md text-gray-600">From Rs.{product.price}.00</p>
         </div>
         <button
           onClick={handelAddToCart}
-          className="border border-gray-500 w-[95%] md:w-[14em] absolute bottom-4 self-center py-1 rounded-lg  text-gray-500 mt-4 cursor-pointer hover:text-black hover:border-black"
+          className="border border-gray-500 w-[80%] md:w-[14em] max-md:text-sm absolute bottom-4 self-center py-1 rounded-lg  text-gray-500 mt-4 cursor-pointer hover:text-black hover:border-black"
         >
           Add To Cart
         </button>
