3ef86f05dcd9715784d03dc5b0e8f2ff9a0cc9cc
Ashish <ashishgupta7206@gmail.com>
2024-09-05 15:09:28 +0530

    search bar animation added

--- src/components/navbar/Search.tsx
+++ src/components/navbar/Search.tsx
@@ -3,6 +3,8 @@ import React, { useState, useEffect } from "react";
 import { IoIosSearch } from "react-icons/io";
 import { IoCloseOutline } from "react-icons/io5";
 import SearchCard from "./SearchCard";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 interface SearchProps {
   closeSearch: () => void;
@@ -33,11 +35,50 @@ const Search: React.FC<SearchProps> = ({ closeSearch }) => {
 
     fetchData();
   }, [searchText]);
-  console.log(limitedData, "search Data");
+  // console.log(limitedData, "search Data");
+  // animation work for the search bar
+  useGSAP(() => {
+    const tl = gsap.timeline();
+    tl
+      // .from("#searchDiv", {
+      //   duration: 0.4,
+      //   y: -100,
+      //   opacity: 0,
+      //   // ease:"elastic.in",
+      // })
+      .from("#inputBox", {
+        duration: 0.4,
+        x: 1000,
+        // opacity: 0,
+        ease: "circ.out",
+      });
+    gsap.from("#resultBox", {
+      duration: 1,
+      y: 100,
+      opacity: 0,
+      ease: "elastic.out",
+    });
+  });
+  // close button clicked animation
+  function closeBar() {
+    gsap.to("#inputBox", {
+      duration: 0.4,
+      x: 1000,
+      // opacity: 0,
+      ease: "circ.in",
+    });
+    setTimeout(() => {
+      closeSearch();
+    },400);
+  }
+
   return (
-    <div className="z-[1001]">
+    <div id="searchDiv" className="z-[1001]">
       <div className="h-[7em] bg-gray-800 flex items-center justify-center">
-        <div className="w-[70vw] lg:w-[50em] h-[2em] sm:h-[3em] relative">
+        <div
+          id="inputBox"
+          className="w-[70vw] lg:w-[50em] h-[2em] sm:h-[3em] relative"
+        >
           <input
             type="text"
             className="rounded-lg w-full h-full bg-gray-800 text-white text-sm sm:text-lg p-1 px-3 outline-none border border-gray-400 focus:border-white focus:border-2"
@@ -49,9 +90,14 @@ const Search: React.FC<SearchProps> = ({ closeSearch }) => {
             <IoIosSearch size={24} />
           </div>
           {searchText && (
-            <div className="w-[70vw] lg:w-[50em] h-[60vh] bg-gray-800 mx-auto absolute border border-t-0 border-gray-400 rounded-lg p-5 overflow-y-auto ">
-                <p className="text-md text-gray-400 uppercase w-[65%] mx-auto ">Products / {searchText}</p>
-                <div className="w-[65%] mx-auto h-[0.01em] bg-gray-400 my-3"></div>
+            <div
+              id="resultBox"
+              className="w-[70vw] lg:w-[50em] h-[60vh] bg-gray-800 mx-auto absolute border border-t-0 border-gray-400 rounded-lg p-5 overflow-y-auto "
+            >
+              <p className="text-md text-gray-400 uppercase w-[65%] mx-auto ">
+                Products / {searchText}
+              </p>
+              <div className="w-[65%] mx-auto h-[0.01em] bg-gray-400 my-3"></div>
               <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 gap-y-5 mt-7 w-fit h-fit my-auto mx-auto">
                 {limitedData.map((item, index) => (
                   <div key={index} className="">
@@ -63,7 +109,7 @@ const Search: React.FC<SearchProps> = ({ closeSearch }) => {
           )}
         </div>
         <IoCloseOutline
-          onClick={closeSearch}
+          onClick={closeBar}
           className="text-white cursor-pointer ml-2 "
           size={30}
         />
--- src/components/splash/Splash.tsx
+++ src/components/splash/Splash.tsx
@@ -1,8 +1,8 @@
 // /components/Splash.tsx
 import React, { useEffect, FC } from "react";
-import {
-  Dancing_Script
-} from "@next/font/google";
+import { Dancing_Script } from "@next/font/google";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 interface SplashProps {
   onTimeout: () => void;
 }
@@ -11,15 +11,25 @@ const Splash: FC<SplashProps> = ({ onTimeout }) => {
   useEffect(() => {
     const timer = setTimeout(() => {
       onTimeout();
-    },900);
+    }, 900);
 
     return () => clearTimeout(timer);
   }, [onTimeout]);
-
+  // useGSAP(() => {
+  //   const tl = gsap.timeline();
+  //   tl.to("#tagLine", {
+  //     scale : 3000,
+  //     duration: 0.9,
+  //     delay : 0.9,
+  //     ease : "power4.in"
+  //   });
+  // });
   return (
-    <div className="bg-black absolute top-0 h-[100vh] text-white w-[100vw] flex items-center justify-center">
-      <div className={Dancing.className}>
-        <p className="text-2xl sm:text-3xl 2xl:text-4xl italic font-bold zoom">Print Your Imagination</p>
+    <div className="bg-black absolute top-0 h-[100vh] overflow-hidden text-white w-[100vw] flex items-center justify-center">
+      <div id="tagLine" className={Dancing.className}>
+        <p className="text-2xl sm:text-3xl 2xl:text-4xl italic font-bold zoom">
+          Print Your Imagination
+        </p>
       </div>
     </div>
   );
