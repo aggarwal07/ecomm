b11c9eb83e07a8fb6600e7e7b32a0282e533ba82
Aggarwal07 <shivamaggarwal179@gmail.com>
2024-10-26 02:21:08 +0530

    landing done

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -17,7 +17,8 @@ import { BiSolidOffer } from "react-icons/bi";
 import CustomCreate from "./CustomCreate";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-
+import { Dancing_Script } from "@next/font/google";
+const Dancing = Dancing_Script({ subsets: ["latin"] });
 const LandingSection = () => {
   const dispatch = useAppDispatch();
   //Product Api data fetching
@@ -81,9 +82,9 @@ const LandingSection = () => {
   //gsap work starts here
   const [currentImage, setCurrentImage] = useState(0);
   const images = [
-    '/Images/home/landing4.gif',
-    '/Images/home/landing5.jpg',
-    '/Images/home/landing6.jpg',
+    "/Images/home/landing4.gif",
+    "/Images/home/landing7.jpg",
+    "/Images/home/landing8.png",
   ];
   useGSAP(() => {
     const tl = gsap.timeline();
@@ -94,11 +95,12 @@ const LandingSection = () => {
       ease: "elastic.out",
     })
       .to("#landingImg", {
-        filter: "blur(6px)",
+        filter: "blur(10px)",
       })
       .from(
         "#landingText p",
         {
+          delay: 0.7,
           y: 200,
           duration: 0.7,
           ease: "power1.out",
@@ -108,24 +110,34 @@ const LandingSection = () => {
         "1"
       )
       .to("#landingImg", {
-        delay : 0.7,
+        delay: 0.7,
         filter: "blur(0px)",
-        duration : 0.7
+        duration: 0.7,
       })
-      .to("#landingText #head", {
-        delay : 0.7,
-        y : -500,
-        x : -350,
-        scale : 0,
-        ease: "power1.out",
-        duration : 1.5,
-      },'3').to("#landingText #tagline", {
-        delay : 0.7,
-        opacity : 0,
-        duration : 0.7,
-      },'3').to("#landingImg", { opacity: 0, duration: 0.5 }) // Fade out
+      .to(
+        "#landingText #head",
+        {
+          delay: 0.7,
+          y: -500,
+          x: -350,
+          scale: 0,
+          ease: "power1.out",
+          duration: 1.5,
+        },
+        "3"
+      )
+      .to(
+        "#landingText #tagline",
+        {
+          delay: 0.7,
+          opacity: 0,
+          duration: 0.7,
+        },
+        "3"
+      )
+      .to("#landingImg", { opacity: 0, duration: 0.5 }) // Fade out
       .call(() => setCurrentImage((prev) => (prev + 1) % images.length)) // Change the image
-      .to("#landingImg", { opacity: 1, duration: 0.5 , width : "80%" }); // Fade in
+      .to("#landingImg", { opacity: 1, duration: 0.5 }); // Fade in
   });
   //gsap work ends here
   return (
@@ -134,11 +146,14 @@ const LandingSection = () => {
         {/*landing image */}
         <div className="bg-[#111111]">
           <div className="w-full  mx-auto  overflow-hidden relative bg-[#111111] flex justify-center ">
-            <div className="absolute z-[1001] w-[100vw] top-[17vh]">
+            <div className="absolute z-[1001] w-[100vw] top-[10vh]">
               <div
                 id="landingText"
-                className="w-[50vw] flex flex-col items-center gap-[2em] text-center mx-auto"
+                className={`w-[50vw] flex flex-col items-center text-center mx-auto ${Dancing.className}`}
               >
+                <p id="tagline" className="text-4xl uppercase">
+                  Bring Your Imagination To Life{" "}
+                </p>
                 <p
                   id="head"
                   style={{ fontSize: "200px" }}
@@ -146,17 +161,14 @@ const LandingSection = () => {
                 >
                   Canvify
                 </p>
-                <p id="tagline" className="text-4xl uppercase">
-                  Elevate your space with custom designs and prints.
-                </p>
-                <p id="tagline" className="text-4xl uppercase">
+                {/* <p id="tagline" className="text-4xl uppercase">
                   From posters and wall canvases to mousepads and keyboard
                   skins, we bring your ideas to life with quality prints.
-                </p>
-                <p id="tagline" className="text-4xl uppercase">
+                </p> */}
+                {/* <p id="tagline" className="text-4xl uppercase">
                   Let Canvify add creativity and artistry to every corner of
                   your home or office.
-                </p>
+                </p> */}
               </div>
             </div>
             <Image
